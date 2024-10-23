271d60b95e379255c150e6364242fb2c7cbafb78
abrowne2 <adamo.browne@gmail.com>
2024-10-22 18:29:07 -0700

    no

--- autogen/oai/client.py
+++ autogen/oai/client.py
@@ -861,9 +861,6 @@ class OpenAIWrapper:
                 ollama_ResponseError,
                 bedrock_BotoCoreError,
                 bedrock_ClientError,
-                cerebras_AuthenticationError,
-                cerebras_InternalServerError,
-                cerebras_RateLimitError,
             ):
                 logger.debug(f"config {i} failed", exc_info=True)
                 if i == last:
